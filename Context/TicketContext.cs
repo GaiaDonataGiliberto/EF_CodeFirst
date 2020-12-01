8d5683a5b0a3ed28851775147c69d7257617cc1b
Gaia Donata Giliberto <gaia.giliberto@gmail.com>
2020-12-01 10:09:30 +0100

    Add project files.

---
+++ Context/TicketContext.cs
@@ -0,0 +1,31 @@
+﻿using EF_CodeFirst.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EF_CodeFirst.Context
+{
+    class TicketContext : DbContext
+    {
+        DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) //forzo il builder a usare la directory che è 
+                //  in esecuzione quando si fa la build, così trova il file json anche quando farò l'assembly
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("TicketDb");
+            // OPPURE config.GetSection("ConnectionStrings")["TicketDb"];
+
+            optionBuilder.UseSqlServer("connString");
+        
+        }
+
+    }
+}
---
+++ Model/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_CodeFirst.Model
+{
+    public class Ticket
+    {
+        // seguo convenzione EF, questa proprietà viene riconosciuta come chiave primaria
+        public int Id { get; set; }
+
+        public DateTime IssueDate { get; set; }
+
+        public string Description { get; set; }
+
+        public string Title { get; set; }
+
+        public string Category { get; set; }
+
+        public string State { get; set; }
+
+
+
+    }
+}
